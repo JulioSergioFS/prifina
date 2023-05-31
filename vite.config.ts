273fb36fde61cac0c0261d382984be0c26b32952
JulioSergioFS <juliosergio010302@gmail.com>
2023-05-31 14:34:09 -0300

    change website name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/forever-learning/",
+  base: "/prifina/",
   build: {
     outDir: "build",
   },
